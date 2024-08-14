d34545a9ef088678d6a1b64f0e09dbdb88b20228
runner <runner@berkeley.edu>
2024-08-13 17:29:41 -0700

    wip

--- lua/plugins/darcula-theme.lua
+++ lua/plugins/darcula-theme.lua
@@ -1,10 +1,11 @@
-  return {
-    'sahglie/darcula-dark.nvim',  priority = 1000,
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter',
-    },
-    config = function()
-      --require("darcula-dark").setup({})
-      vim.cmd.colorscheme "darcula-dark"
-    end
-  }
+return {
+  'sahglie/darcula-dark.nvim',
+  priority = 1000,
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter',
+  },
+  config = function()
+    --require("darcula-dark").setup({})
+    vim.cmd.colorscheme 'darcula-dark'
+  end,
+}
---
+++ lua/plugins/gruvbox-theme.lua
@@ -0,0 +1,11 @@
+--return {
+--  'morhetz/gruvbox',
+--  dependencies = {
+--    'nvim-treesitter/nvim-treesitter',
+--  },
+--  config = function()
+--    vim.cmd.colorscheme 'gruvbox'
+--  end,
+--}
+
+return {}

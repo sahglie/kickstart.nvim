ee1fbc0114c7464436f88b178810bf36a1ff704f
runner <runner@berkeley.edu>
2024-08-15 08:40:51 -0700

    wip

--- lua/plugins/nvim-tmux-navigator.lua
+++ lua/plugins/nvim-tmux-navigator.lua
@@ -1,3 +1,7 @@
 return {
   'christoomey/vim-tmux-navigator',
+  vim.keymap.set('n', 'C-l', ':TmuxNavigateLeft<CR>'),
+  vim.keymap.set('n', 'C-j', ':TmuxNavigateDown<CR>'),
+  vim.keymap.set('n', 'C-k', ':TmuxNavigateUp<CR>'),
+  vim.keymap.set('n', 'C-h', ':TmuxNavigateRight<CR>'),
 }

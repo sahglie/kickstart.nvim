840a726fee48536433fef4ce1c2d2adee68037b2
runner <runner@berkeley.edu>
2024-08-15 10:10:51 -0700

    wip

--- lua/plugins/vim-test.lua
+++ lua/plugins/vim-test.lua
@@ -1,7 +1,17 @@
+local function docker_transform(cmd)
+  return 'docker compose exec web ' .. cmd
+end
+
 return {
-  vim.keymap.set('n', '<leader>t', ':TestNearest<CR>'),
-  vim.keymap.set('n', '<leader>T', ':TestFile<CR>'),
-  vim.keymap.set('n', '<leader>a', ':TestSuite<CR>'),
-  vim.keymap.set('n', '<leader>l', ':TestLast<CR>'),
-  vim.keymap.set('n', '<leader>g', ':TestVisit<CR>'),
+  'vim-test/vim-test',
+  config = function()
+    vim.keymap.set('n', '<leader>t', ':TestNearest<CR>')
+    vim.keymap.set('n', '<leader>T', ':TestFile<CR>')
+    vim.keymap.set('n', '<leader>a', ':TestSuite<CR>')
+    vim.keymap.set('n', '<leader>l', ':TestLast<CR>')
+    vim.keymap.set('n', '<leader>g', ':TestVisit<CR>')
+
+    vim.g['test#custom_transformations'] = { docker = docker_transform }
+    vim.g['test#transformation'] = 'docker'
+  end,
 }
